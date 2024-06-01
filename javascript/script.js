203ef2e2d170992e17a0095b43fefe7d4541e3a7
Victor Almeida <157659240+V1774R@users.noreply.github.com>
2024-05-31 22:27:56 -0300

    Adicionado animação

---
+++ javascript/script.js
@@ -0,0 +1,33 @@
+const sessoes = document.querySelectorAll('section');
+const experiencias = document.querySelectorAll('.experiencia__item');
+
+
+const myObserver = new IntersectionObserver((elementos)=>{
+    elementos.forEach(elemento => {
+        console.log(elemento)
+        if(elemento.isIntersecting){
+            elemento.target.classList.add('show');
+        }else{
+            //elemento.target.classList.remove('show');
+        }
+    })
+});
+
+sessoes.forEach((sessao)=>{
+    myObserver.observe(sessao);
+})
+
+
+const myObserver2 = new IntersectionObserver((elementos)=>{
+    elementos.forEach((elemento) => {
+        if(elemento.isIntersecting){
+            elemento.target.classList.add('show');
+        }else{
+            //elemento.target.classList.remove('show');
+        }
+    })
+})
+
+experiencias.forEach(experiencia => {
+    myObserver2.observe(experiencia);
+})
